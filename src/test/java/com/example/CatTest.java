f596501d633d99a0fb30dc5aeae6f3c6ad2e4f7d
olgaburmakina <ol.guseva199@yandex.ru>
2022-12-01 14:32:00 +0700

    Commit Sprint_6

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class CatTest extends TestCase {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void testGetSound() {
+        var cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFood() throws Exception {
+        when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        var cat = new Cat(feline);
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), cat.getFood());
+    }
+}
---
+++ src/test/java/com/example/LionTest.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+
+@RunWith(Parameterized.class)
+
+public class LionTest extends TestCase {
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    Feline feline = Mockito.mock(Feline.class);
+
+    private final boolean expected;
+
+    private final String gender;
+
+    public LionTest(boolean expected, String gender) {
+        this.expected = expected;
+        this.gender = gender;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][] {
+                {true,"Самец"},
+                {false, "Самка"},
+        };
+    }
+
+    @Test
+    public void doesHaveMane() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        assertEquals(true, lion.doesHaveMane());
+    }
+
+    @Test
+    public void getFood() throws Exception {
+        when(feline.getFood("Хищник")).thenReturn(List.of("one", "two", "three"));
+        var lion = new Lion(gender, feline);
+        assertEquals(List.of("one", "two", "three"), lion.getFood());
+    }
+
+    @Test
+    public void checkException() {
+        Exception actualException = assertThrows(Exception.class, () ->
+                new Lion("Чиполлино", feline));
+        assertEquals("Используйте допустимые значения пола животного - самец или самка", actualException.getMessage());
+    }
+}
+
+
